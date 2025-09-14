d00e25efb384cc3ed21c4ef846e3ad40942d3a17
kaifuddin009 <kaifuddin01@email.com>
2025-09-14 15:41:02 +0530

    add root route

--- server/src/server.js
+++ server/src/server.js
@@ -14,6 +14,10 @@ app.use(cors());
 app.use(express.json());
 
 //Routes
+app.get("/", (req, res) => {
+  res.send("Server is running ✅");
+});
+
 app.use("/api/admin",adminRouter)
 app.use("/api/blog",blogRouter)
 app.use("/api/comment/",commentRouter)
